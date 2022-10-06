ad8994ffd4fbc201b7de4caef6d373b874c33bbe
Shankar Y Bhavani <ybshankar010@users.noreply.github.com>
2022-10-06 11:01:28 +0530

    add startup command in entry point script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#./manage.py collectstatic --noinput
+./manage.py collectstatic --noinput
 # i commit my migration files to git so i dont need to run it on server
 # ./manage.py makemigrations app_name
 ./manage.py makemigrations
 
 ./manage.py migrate
+
+gunicorn --reload --bind 0.0.0.0:8000 mysite.wsgi:application
