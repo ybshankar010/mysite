f9eebfea4cf01b1e922d9e90739fc4361356a8af
Shankar Y Bhavani <ybshankar010@users.noreply.github.com>
2022-10-19 14:16:43 +0530

    Add files via upload

--- main/forms.py
+++ main/forms.py
@@ -5,7 +5,7 @@ from .models import ContactProfile
 class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactProfile
-        fields = ("name", "email", "message")
+        fields = "__all__"
 
     name = forms.CharField(
         max_length=100,
@@ -17,6 +17,12 @@ class ContactForm(forms.ModelForm):
         max_length=254,
         required=True,
         widget=forms.TextInput(attrs={"placeholder": "*Email address"}),
+    )
+
+    subject = forms.CharField(
+        max_length=200,
+        required=True,
+        widget=forms.Textarea(attrs={"placeholder": "*subject.."}),
     )
 
     message = forms.CharField(
--- main/views.py
+++ main/views.py
@@ -1,8 +1,21 @@
+import email
+from email import message
+from telnetlib import STATUS
 from django.shortcuts import render
 from django.contrib import messages
-from .models import UserProfile, Blog, Portfolio, Testimonial, Certificate
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from .models import (
+    ContactProfile,
+    UserProfile,
+    Blog,
+    Portfolio,
+    Testimonial,
+    Certificate,
+)
 
 from django.views import generic
+from django.http import HttpResponse
 from .forms import ContactForm
 
 
@@ -14,8 +27,8 @@ class IndexView(generic.TemplateView):
 
         testimonials = Testimonial.objects.filter(is_active=True)
         certificates = Certificate.objects.filter(is_active=True)
-        blogs = Blog.objects.filter(is_active=True)
-        portfolio = Portfolio.objects.filter(is_active=True)
+        blogs = Blog.objects.all
+        portfolio = Portfolio.objects.all
 
         context["testimonials"] = testimonials
         context["certificates"] = certificates
@@ -25,14 +38,22 @@ class IndexView(generic.TemplateView):
 
 
 class ContactView(generic.FormView):
-    template_name = "main/contact.html"
+    template_name = "main/index.html"
     form_class = ContactForm
     success_url = "/"
 
     def form_valid(self, form):
-        form.save()
+        print("In there")
+        print(form.data)
+        contactDetails = ContactProfile(
+            name=form.data["name"],
+            email=form.data["email"],
+            subject=form.data["subject"],
+            message=form.data["message"],
+        )
+        contactDetails.save()
         messages.success(self.request, "Thank you, we will be in touch soon")
-        return super.form_valid(form)
+        return super().form_valid(form)
 
 
 class PortfolioView(generic.ListView):
@@ -46,18 +67,18 @@ class PortfolioView(generic.ListView):
 
 class PortfolioDetailView(generic.DetailView):
     model = Portfolio
-    template_name = "main/portfolio-detail.html"
+    template_name = "main/portfolio-details.html"
 
 
 class BlogView(generic.ListView):
     model = Blog
-    template_name = "main/blog.html"
+    template_name = "main/blog-grid.html"
     paginate_by = 10
 
     def get_queryset(self):
-        return super().get_queryset().filter(is_active=True)
+        return super().get_queryset().all
 
 
 class BlogDetailView(generic.DetailView):
     model = Blog
-    template_name = "main/blog-detail.html"
+    template_name = "main/blog-single.html"
